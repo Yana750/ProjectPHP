6a42e9696017aa37d6a2e7922744cc596589e3d9
yana <vasyana750@gmail.com>
2023-09-19 00:40:07 +0700

    five

--- education/index.php
+++ education/index.php
@@ -1,6 +1,41 @@
-<?php
+<html>
+<head>
+    <title>Laba_Vasileva</title>
+</head>
+<body>
+<p>Hello world!</p>
 
-$a=10;
-$b=5;
+<form>
+    <div class="container">
+        <?php if ($_SERVER['REQUEST_METHOD'] === 'GET') { ?>
+            <form action="form.php" method="post">
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Firstname</label>
+                    <input type="text" class="form-control" name="Имя" id="exampleFormControlInput1" value="<?php echo $_POST['Имя'] ?? '' ?>" placeholder="Введите Ваше Имя">
+                </div>
 
-echo $a+$b;
+                <div class="mb-3">
+                    <label for="exampleFormControlInput2" class="form-label">Lastname</label>
+                    <input type="text" class="form-control" name="Фамилия" id="exampleFormControlInput2" value="<?php echo $_POST['Фамилия'] ?? '' ?>" placeholder="Введите Вашу Фамилию">
+                </div>
+
+                <div class="mb-3">
+                    <input type="submit" class="btn btn-primary" value="Сохранить">
+                </div>
+            </form>
+            <?php
+        }
+        ?>
+    </div>
+    <div class="container">
+        <?php
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            echo '<b>Имя</b> = ' . ($_POST['Имя'] ?? '') . '<br>';
+            echo 'Фамилия = ' . ($_POST['Фамилия'] ?? '');
+        }
+        ?>
+    </div>
+</form>
+
+</body>
+</html>
